7d1165599304c697657f7cfbdc10f897c6c38541
data_will <ws.data.engineering@gmail.com>
2024-07-11 18:24:03 -0500

    Updated feature branch

--- 20-Stu_Data-Attributes/Unsolved/assets/js/script.js
+++ 20-Stu_Data-Attributes/Unsolved/assets/js/script.js
@@ -1,9 +1,17 @@
 const container = document.querySelector('.container');
 
+
+function showDefaultNumbers() {
+  for(let num = 0; num < container.children.length; num += 2) {
+    container.children[num].innerHTML = `${num + 1}`;
+  };
+};
+
 container.addEventListener('click', function (event) {
   const element = event.target;
   const num = element.dataset.number;
   container.children[num - 1].innerHTML = `${num}`;
   element.dataset.state = `displayed`;
-  // TODO: Complete function
 });
+
+showDefaultNumbers();
